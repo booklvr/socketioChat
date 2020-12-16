584a6bfc9ca014d400cc2ba0725ad941782c89df
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-12-16 16:27:49 +0900

    going home

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id);
+  console.log(product);
+  <Link className='btn btn-dark'></Link>;
+  return <Fragment></Fragment>;
+};
+
+export default ProductScreen;
