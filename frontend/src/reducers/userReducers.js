d70dccf6b14d1379d28257a1bdacfd8ddec5dcf3
Nicholas de Waal <nick.a.dewaal@gmail.com>
2020-12-24 16:29:11 +0900

    going home

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,12 @@ import {
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -34,6 +40,36 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: payload }
     case USER_REGISTER_FAIL:
+      return { loading: false, error: payload }
+    default:
+      return state
+  }
+}
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case USER_DETAILS_SUCCESS:
+      return { loading: false, user: payload }
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: payload }
+    default:
+      return state
+  }
+}
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  const { type, payload } = action
+
+  switch (type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, userInfo: payload }
+    case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: payload }
     default:
       return state
